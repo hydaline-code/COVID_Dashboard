4d981264e8bf8567032cf25fbda24f25f61c1f2f
hydaline-code <djougancharlen@.com>
2024-01-06 16:32:23 +0100

    Add storage active

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
 import './App.css';
 
@@ -19,16 +19,32 @@ function App() {
     { value: 'SK', label: 'Saskatchewan' },
     { value: 'YT', label: 'Yukon' },
   ];
+
+  // state to store the active locations
+  /* eslint-disable no-unused-vars */
+  const [activeLocation, setActiveLocation] = useState('AB');
+  const [lastUpdated, setlastUpdated] = useState(' ');
+
   // return statement goes below this
   return (
     <div className="App">
       <h1>COVID 19 Dashboard </h1>
       <div className="dashboard-container">
-        <div className="dashboard-menu" />
-        <Select
-          options={locationList}
-          className="dasboard-select"
-        />
+        <div className="dashboard-menu">
+          <Select
+            options={locationList}
+            onChange={(selectedOption) => setActiveLocation(selectedOption.value)}
+            defaultValue={locationList.filter(
+              (options) => options.value === activeLocation,
+            )}
+            className="dasboard-select"
+          />
+          <p className="update-date">
+            {' '}
+            Last Updated :
+            {lastUpdated}
+          </p>
+        </div>
         <div className="dashboard-summary" />
       </div>
     </div>
